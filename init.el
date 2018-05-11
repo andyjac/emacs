96bdcdaf774d72874bbd5779f2cd943897da0b7e
Andrew Jacobson <andrew.isaac.jacobson@gmail.com>
2018-05-11 16:22:44 -0700

    Update config

--- init.el
+++ init.el
@@ -47,4 +47,4 @@
     (setq gc-cons-threshold 800000))
 
   (add-hook 'minibuffer-setup-hook #'my-minibuffer-setup-hook)
-  (add-hook 'minibuffer-exit-hook #'my-minibuffer-exit-hook))
+  (add-hook 'minibuffer-exit-hook  #'my-minibuffer-exit-hook))
